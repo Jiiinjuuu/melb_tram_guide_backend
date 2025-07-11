b0e54bb8981207e6aee93a7047b5cb928a644140
Jiiinjuuu <jinju1010@yu.ac.kr>
2025-07-11 16:59:47 +0900

    session_start()보다 cors.php 포함 먼저

--- public/login.php
+++ public/login.php
@@ -1,8 +1,26 @@
 <?php
-// 세션 시작
+
+$allowedOrigins = [
+    "http://localhost:3000",
+    "https://melb-stamp-tour.netlify.app"
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 session_start();
 
-require_once __DIR__ . '/../includes/cors.php';
 require_once __DIR__ . '/db_connect.php';
 
 header("Content-Type: application/json; charset=utf-8");
